35edf39fbc6af0b74d08f12772424edf76721e42
dvic <info@dvic.io>
2021-06-03 11:48:35 +0200

    Fix error translation

--- lib/my_app_web/live/page_live.ex
+++ lib/my_app_web/live/page_live.ex
@@ -29,6 +29,7 @@ defmodule MyAppWeb.PageLive do
     {:ok, meta, socket}
   end
 
+  def error_to_string(:external_client_failure), do: "External client failure"
   def error_to_string(:too_large), do: "Too large"
   def error_to_string(:too_many_files), do: "You have selected too many files"
   def error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
